ced7e187d462e3bc64f1bb818d715a0ffa96abda
Pedro Rabinovitch <pedro@spaceapegames.com>
2013-09-08 09:41:31 +0100

    Initial import

---
+++ Unity/ChainJam/Assets/Scripts/Floating.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent(Rigidbody)]
+public class Floating : MonoBehaviour {
+	public float Buoyancy = 9.8f;
+	public float MinBuoyancy = 3.0f;
+	public float MaxBuoyancy = 13.0f;
+
+	public float InflateRate = 5f;
+	public float DeflateRate = 0.05f;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void Update() {
+		if (Input.GetKey(KeyCode.A))
+		{
+			Buoyancy += InflateRate * Time.deltaTime;
+		}
+		else
+		{
+			Buoyancy -= (Buoyancy * DeflateRate) * Time.deltaTime;
+		}
+		Buoyancy = Mathf.Clamp(Buoyancy, MinBuoyancy, MaxBuoyancy);
+
+		gameObject.transform.localScale = Vector3.one * (Buoyancy / 9.8f);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		var force = Buoyancy;
+
+		rigidbody.AddForce(force * Vector3.up);
+	}
+}
---
+++ Unity/ChainJam/Assets/Scripts/StringRender.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StringRender : MonoBehaviour {
+	SpringJoint joint;
+	LineRenderer lineRenderer;
+
+
+	public Color c1 = Color.white;
+	public Color c2 = Color.white;
+	public int lengthOfLineRenderer = 2;
+	void Start() {
+		joint = GetComponent<SpringJoint>();
+		LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+		lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+		lineRenderer.SetColors(c1, c2);
+		lineRenderer.SetWidth(0.05F, 0.05F);
+		lineRenderer.SetVertexCount(lengthOfLineRenderer);
+	}
+	/**
+	void Update() {
+		LineRenderer lineRenderer = GetComponent<LineRenderer>();
+		int i = 0;
+		while (i < lengthOfLineRenderer) {
+			Vector3 pos = new Vector3(i * 0.25F, Mathf.Sin(i*0.5f + Time.time), 0);
+			lineRenderer.SetPosition(i, pos);
+			i++;
+		}
+	}
+	/*/
+
+	// Update is called once per frame
+	void Update () {
+		if (joint != null)
+		{
+			var body = joint.connectedBody;
+			if (body != null)
+			{
+				var lineRenderer = GetComponent<LineRenderer>();
+				lineRenderer.SetPosition(0, body.transform.position);
+				lineRenderer.SetPosition(1, transform.position);
+//				for (int i=0; i < lengthOfLineRenderer; i++) {
+//				}
+				Debug.DrawLine(body.transform.position, transform.position, Color.magenta);
+			}
+		}
+	}
+	/**/
+}
